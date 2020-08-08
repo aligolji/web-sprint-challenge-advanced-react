c9e72b12f9ef53f98ebc8de7287cc0c6fe616b8b
unknown <ali-golji@lambdastudents.com>
2020-08-07 21:36:28 -0700

    build of tests complete. tests pass. purposeful fail attempts successful. MVP complete.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from "../App";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const header = screen.getByText('Checkout Form');
+    expect(header).toBeInTheDocument();
+    // expect(header).not.toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+    const checkoutButton = screen.getByRole('button', { name: 'Checkout' });
+
+    fireEvent.click(checkoutButton);
+
+    const successmsg = await screen.findByTestId('successMessage');
+
+    expect(successmsg).toBeInTheDocument();
+    // expect(successmsg).not.toBeInTheDocument();
+});
+
